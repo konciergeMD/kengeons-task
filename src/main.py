7a8a77d44eb994bfbd071bea9a9262dfbd710adc
Kamil Samaj <kamil.samaj@accolade.com>
2019-07-10 20:23:46 +0200

    pytest test passing

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 def solution(A: list):
-    pass
+    A.sort()
+    len_a = len(A)
+    for i in range(len_a - 1):
+        if A[i] > 0:
+            if (A[i+1] - A[i]) > 1:
+                return A[i] + 1
+            elif i == len_a - 2:
+                return A[i+1] + 1
+
+    return 1
