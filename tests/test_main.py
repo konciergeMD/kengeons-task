765252184b6ccc42b571f68f73e2a7ec4998177d
Jakub Zeman <jakub.zeman@accolade.com>
2021-05-07 13:10:42 +0200

    Better UNIT tests added

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,3 +11,18 @@ class TestMain(unittest.TestCase):
         self.assertEqual(1, solution([-2, -1, 4]))
         self.assertEqual(1, solution([-2, -1, 0, 4]))
         self.assertEqual(1, solution([-1, -3]))
+
+    @pytest.mark.timeout(6)
+    def test_big_numbers(self):
+        f = open("tests/list.txt", "r")
+        big_array = f.readline()
+        f.close()
+        self.assertEqual(50997, solution(eval(big_array)))
+
+    @pytest.mark.timeout(6)
+    def test_big_array(self):
+        f = open("tests/list_from_1.txt", "r")
+        big_array = f.readline()
+        f.close()
+        self.assertEqual(100000, solution(eval(big_array)))
+
